4ab71713983d20fbf561db7276afaa032c6a91bc
Eslam Osama Ahmed <37809837+EslamOsamaAhmed@users.noreply.github.com>
2019-04-19 15:48:46 +0200

    new backup
    new backup

---
+++ IA_Project/IA_Project/Controllers/SProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IA_Project.Controllers
+{
+    public class SProfileController : Controller
+    {
+        // GET: SProfile
+        public ActionResult SProfile_View()
+        {
+            return View();
+        }
+    }
+}
