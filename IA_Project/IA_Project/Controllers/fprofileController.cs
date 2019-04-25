438389a3471af6a91474862cdd54f7c26e71d7c9
Eslam Osama Ahmed <37809837+EslamOsamaAhmed@users.noreply.github.com>
2019-04-25 06:31:23 +0200

    rezk
    rezk

--- IA_Project/IA_Project/Controllers/fprofileController.cs
+++ IA_Project/IA_Project/Controllers/fprofileController.cs
@@ -12,11 +12,36 @@ namespace IA_Project.Controllers
 {
     public class fprofileController : Controller
     {
+<<<<<<< HEAD
         
 
         public ActionResult fprofile()
         {
             return View();
+=======
+        // GET: fprofile
+        public ActionResult fprofile()
+        {
+            /*test actor = new test { id = 12 };
+
+            ViewData["test"] = actor;
+
+            ViewBag.myactor = actor;*/
+
+            DataBaseFuncController x = new DataBaseFuncController();
+            S_ACTORS y = new S_ACTORS { ACTOR_ID = 1000, AROLE = "Director" };
+
+            var val = x.GetActorDataByID(1000);
+
+            var ActorID = val.ACTOR_ID;
+            var ActorName = val.FNAME + val.LNAME;
+
+            ViewBag.ID = ActorName;
+
+            return View();
+
+                
+>>>>>>> parent of 059f266... Select Project From Database
         }
 
         [HttpPost]
