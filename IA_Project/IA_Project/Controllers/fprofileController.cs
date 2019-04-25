451ddf3a7d175b7d1014fa212fa45b002e7becbe
Eslam Osama Ahmed <37809837+EslamOsamaAhmed@users.noreply.github.com>
2019-04-25 05:43:02 +0200

    DataBase
    DataBase

--- IA_Project/IA_Project/Controllers/fprofileController.cs
+++ IA_Project/IA_Project/Controllers/fprofileController.cs
@@ -20,14 +20,14 @@ namespace IA_Project.Controllers
         }
 
         [HttpPost]
-        public string prof(string pageid, string pageurl, string actorid)
+        public string prof(string pageurl, string actorid)
         {
             try
             {
                 /*DataBaseFuncController x = new DataBaseFuncController();
             x.page(obj);
             return View();*/
-                S_PAGE s = new S_PAGE() { PAGE_URL = pageurl, ACTOR_ID_P = Int32.Parse(actorid), PAGE_ID = Int32.Parse(pageid) };
+                S_PAGE s = new S_PAGE() { PAGE_URL = pageurl, ACTOR_ID_P = Int32.Parse(actorid)};
                 DataBaseFuncController x = new DataBaseFuncController();
 
                 x.page(s);
